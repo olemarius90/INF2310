cc028120fc90c59b55ad27460c57bf7242675ff8
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2015-01-14 13:58:05 +0100

    Oppdatert versjon, men mangler å kommentere samt stykkvis lineær mapping

--- uke4/gratoneTransformasjoner.m
+++ uke4/gratoneTransformasjoner.m
@@ -2,16 +2,16 @@ clear all
 close all
 
 % Vi importerer et standardbilde fra MATLAB
-I = imread('pout.tif');
+f = imread('pout.tif');
+[n,m] = size(f);
 
 % Bruker vår funksjon myHist til å finne histogrammet
-[h,p,c,c_n] = myHist(I);
-
+[h,p,c,c_n] = myHist(f);
 
 % Vi plotter resultatene
 figure(1)
 subplot(141)
-imshow(I,[]);
+imshow(f,[]);
 title('Bilde');
 xlabel('Gråtone');
 ylabel('Antall piksler');
@@ -31,14 +31,14 @@ xlabel('Gråtone');
 ylabel('Antall piksler');
 
 subplot(144)
-imhist(I);
+imhist(f);
 axis tight
 title('MATLAB histogram');
 xlabel('Gråtone');
 ylabel('Antall piksler');
 
-% Og kan fra resultatene trekke slutningen at vår implementasjon virker
-% korrekt
+% Og kan fra resultatene trekke slutningen at vår implementasjon ser ut til
+% å være riktig
 
 %%
 figure(2)
@@ -64,7 +64,6 @@ ylabel('Antall piksler');
 
 %%
 %Endre "lysheten" brightness
-f = I;%Orginal bildet er i f
 a = 1;
 b = 256/2.5;
 
@@ -97,3 +96,105 @@ subplot(224)
 bar(myHist(g));
 
 
+%%
+%Middelverdi av gråtonene
+
+u1 = mean(f(:));
+
+u2 = 0;
+for i = 1:2^8
+   u2 = u2 + (i-1)*p(i);
+end
+
+u1 
+u2 
+
+%% Varians av gråtonene
+v1 = var(double(f(:)));
+
+v2 = 0;
+v3 = 0;
+for i = 1:2^8
+    v3 = v3 + p(i)*((i-1)-u1)^2;
+    v2 = v2 + (i-1)^2*p(i);
+end
+
+temp = 0;
+for j = 1:2^8
+    temp = temp+(j-1)*p(j);
+end
+v2 = v2 - temp^2;
+
+v1
+v2
+v3
+
+%% Justering av middelverdi og varians
+u_ny = 240
+v_ny = 150
+a = sqrt(v_ny)/sqrt(v1);
+b = u_ny - a*u1;
+
+g = f*a+b;
+
+figure(5)
+subplot(221)
+imshow(f,[]);
+subplot(222)
+imshow(g,[]);
+subplot(223)
+bar(myHist(f));
+subplot(224)
+bar(myHist(g));
+
+
+mean(g(:))
+var(double(g(:)))
+
+
+%% Logaritmiske transformasjoner
+
+g = uint8(40*log(double(1+f)));
+figure(6)
+subplot(221)
+imshow(f,[0 2^8]);
+subplot(222)
+imshow(g,[0 2^8]);
+subplot(223)
+bar(myHist(f));
+subplot(224)
+bar(myHist(g));
+
+%Show a ultrasound image as well
+
+%% "Power-law" (gamma)-transformasjoner
+gamma = 1.09;
+c = 1;
+g = uint8(c*double(f).^gamma);
+
+figure(6)
+subplot(221)
+imshow(f,[0 2^8]);
+subplot(222)
+imshow(g,[0 2^8]);
+subplot(223)
+bar(myHist(f));
+subplot(224)
+bar(myHist(g));
+
+%% Bit-plan oppdeling
+
+layer = 8;
+for i = 1:n
+    for j = 1:m
+        out(i,j) = bitand(f(i,j),2^layer);
+    end
+end
+
+out = out > 0;
+
+figure(6)
+subplot(221)
+imshow(f,[0 2^8]);
+subplot(222)
+imshow(out,[]);
