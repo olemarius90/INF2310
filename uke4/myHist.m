13cb8f9cd4e9aa8d203c47123fa7bcce880a013b
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2015-01-31 18:42:08 +0100

    Lagt til noen kommentarer

--- uke4/myHist.m
+++ uke4/myHist.m
@@ -1,21 +1,41 @@
 function [p,h,c,c_n] = myHist(img)
-%MYHIST Calculates the histogram of an image
+%MYHIST Regner ut gråtonehistogrammet til et bilde. Antar her at det er
+% 256 (2^8) gråtoner i bildet.
+%
+%   @input
+%   img : Input bildet
+%
+%   @output
+%   p   : Det normaliserte histogrammet til bildet
+%   h   : Histogrammet til bildet
+%   c   : Det kumulative histogrammet
+%   c_n : Det normalizerte kumulative histogrammet
 
+% Finner størrelsen til bildet
 [n,m] = size(img);
-h = zeros(1,2^8);
-p = zeros(1,2^8);
-c = zeros(1,2^8);
 
+% Preallokerer variable for å holde resultatene
+h = zeros(1,2^8);   % Histogrammet
+c = zeros(1,2^8);   % Det kumulative histogrammet
+
+% Finner histogrammet ved å summere hvor mange piksler som finnes av hver
+% gråtone. Dette kan helt klart programmeres mer effektivt, men jeg mener
+% denne implementasjonen er relativt intuitiv.
 for i = 1:2^8
     h(i) = sum(sum(uint8(img == i-1)));
 end
 
+% Normaliserer histogrammet
 p = h/(n*m);
 
+% Legger sammen antallet gråtoner "så langt" for å få det kumulative
+% histogrammet som indikerer hvor mange piksler som har gråtone mindre eller
+% lik gråtone i.
 for i = 2:2^8
     c(i) = h(i) + c(i-1);
 end
 
+% Normaliserer det kumulative histogrammet
 c_n = c/(n*m);
 end
 
